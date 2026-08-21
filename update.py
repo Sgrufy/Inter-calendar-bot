6f91e3b7f18e84b7536eacc7a52bfbf029dd03f3
Sgrufy <djalexinside@gmail.com>
2026-08-21 16:31:01 +0200

    Update update.py

--- update.py
+++ update.py
@@ -14,7 +14,7 @@ HEADERS = {
 COMPETITIONS = ['SA', 'CL', 'COI', 'ITC']
 TEAM_ID = 108
 
-# Dizionario canali completo (inclusi Setanta Sports e tutti i precedenti)
+# Dizionario canali completo (con l'aggiunta dei nuovi canali Polsat)
 CANALI_EPG = {
     # Canali originari
     "Eleven Sports 1": "6339",
@@ -87,26 +87,34 @@ CANALI_EPG = {
     "Sport TV 3": "397419",
     "Sport TV 2": "397424",
     "Sport TV 1": "397418",
-    # Altri canali (Football e Astro)
+    # Altri canali
     "SS Football": "463676",
     "S Football": "450846",
     "Astro Football HD": "533442",
     "Astro Football": "399498",
-    # Canali BBC Alba
     "BBC Alba": "12059",
     "BBC Alba HD 1": "486822",
     "BBC Alba HD 2": "537759",
-    # Nuovi canali Setanta Sports aggiunti
     "Setanta Sports 1": "417364",
-    "Setanta Sports 2": "417351"
+    "Setanta Sports 2": "417351",
+    # Nuovi canali Polsat aggiunti con icona TV (inclusi nel gruppo senza pallino blu)
+    "Polsat Sport Premium 6": "452290",
+    "Polsat Sport Premium 5": "449589",
+    "Polsat Sport Premium 4": "449590",
+    "Polsat Sport Premium 3": "408447",
+    "Polsat Sport Premium 2": "7135",
+    "Polsat Sport Premium 1": "7136",
+    "Polsat Sport Extra": "7835"
 }
 
-# Tutti i canali tranne i primi 18 originali avranno il pallino blu
+# I canali esclusi da CANALI_BLU useranno l'icona della TV (📺)
 CANALI_BLU = set(CANALI_EPG.keys()) - {
     "Eleven Sports 1", "Eleven Sports 2", "Eleven Sports 3", "Eleven Sports 4",
     "Canal+ Sport", "Canal+ Sport 2", "Canal+ Extra", "Canal+ 1",
     "Polsat Sport 1", "Polsat Sport 2", "Polsat Sport 3", "TVP Sport",
-    "Cosmote 1", "Cosmote 2", "Max Sport 1", "Max Sport 2", "Nova Sport 1", "Nova Sport 2"
+    "Cosmote 1", "Cosmote 2", "Max Sport 1", "Max Sport 2", "Nova Sport 1", "Nova Sport 2",
+    "Polsat Sport Premium 6", "Polsat Sport Premium 5", "Polsat Sport Premium 4", 
+    "Polsat Sport Premium 3", "Polsat Sport Premium 2", "Polsat Sport Premium 1", "Polsat Sport Extra"
 }
 
 def pulisci_nome(nome):
